3d80a076c149632616bfbe356fdac545c6a0511e
MatchBox Official <112538364+MatchBox-ID@users.noreply.github.com>
2023-05-19 14:10:02 +0700

    update credit.js

--- credit.js
+++ credit.js
@@ -1,1 +1,13 @@
-
+{
+  "owner":"TrailBox"
+  "creator":"Matchbox"
+  "run":"npm start"
+  "apikey":"" // apikey anda
+  "privatekey":"" // private key anda
+  "website":"https://matchboxid.my.id"
+}
+{
+  "servername":"TrailBox"
+  "console.log":"https://web.whatsapp.com"
+  "run":"npm start"
+}
